14ef51f3c282d88ab682d3949d7fc03cc43c039d
Aline Vignoli <nyhv.contato@gmail.com>
2020-10-09 13:10:45 -0300

    interpretacao de codigo 1

---
+++ semana4/lista-de-exercicios/index.js
@@ -0,0 +1,9 @@
+// EXERCICIOS DE INTERPRETAÇÃO DE CÓDIGO (o que faz, como faz, o que é impresso)
+
+// 1.
+/*
+A função conversorDeMoeda serve para converter um valor em dólar para reais.
+Ela recebe um valor em dólar como parâmetro, e através de um prompt pede para o usuário informar o valor da cotação do dólar. Em seguida ele retorna uma string "R$" concatenada com um number, que é o valor em dólar (que é passado como parâmetro) multiplicado pela cotação (informada no prompt).
+Fora da função, logo abaixo é declarada uma variável meuDinheiro que vai guardar o retorno dessa função, passando o valor 100 para o parâmetro valorEmDolar. 
+Mais abaixo, o console irá imprimir a variável meuDinheiro, que vai ser igual à 100 multiplicado pelo número que o usuário digitar no prompt, como o R$ na frente.
+*/
