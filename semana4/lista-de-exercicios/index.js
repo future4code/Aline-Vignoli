3bf686fa2525786cc53202e516d6cfb4cf08a71e
Aline Vignoli <nyhv.contato@gmail.com>
2020-10-11 13:42:57 -0300

    funcoes de array 5

--- semana4/lista-de-exercicios/index.js
+++ semana4/lista-de-exercicios/index.js
@@ -352,3 +352,27 @@ Abaixo estão as correções:
 // gerarEmailsConsultas(consultas)
 // console.log(emailsNaoCanceladas)
 // console.log(emailsCanceladas)
+
+// 5.
+// const contas = [
+// 	{ cliente: "João", saldoTotal: 1000, compras: [100, 200, 300] },
+// 	{ cliente: "Paula", saldoTotal: 7500, compras: [200, 1040] },
+// 	{ cliente: "Pedro", saldoTotal: 10000, compras: [5140, 6100, 100, 2000] },
+// 	{ cliente: "Luciano", saldoTotal: 100, compras: [100, 200, 1700] },
+// 	{ cliente: "Artur", saldoTotal: 1800, compras: [200, 300] },
+// 	{ cliente: "Soter", saldoTotal: 1200, compras: [] }
+// ]
+
+// const atualizarSaldoTotal = (array)=>{
+// 	array.forEach((conta)=>{
+// 		let soma = 0
+// 		conta.compras.forEach((compra)=>{
+// 			soma += compra
+// 		})
+
+// 		conta.saldoTotal -= soma
+// 	})
+// }
+
+// atualizarSaldoTotal(contas)
+// console.log(contas)
