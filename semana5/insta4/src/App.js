4c8087fcc8e4f80198256863722cbafb5fb897fc
Aline Vignoli <nyhv.contato@gmail.com>
2020-10-16 13:54:37 -0300

    criei um array para armazenar os posts e exibi na tela

--- semana5/insta4/src/App.js
+++ semana5/insta4/src/App.js
@@ -5,10 +5,41 @@ import fotoPerfilAline from './img/perfil-aline.jpg';
 import logoLabenu from './img/labenu-logo.png';
 
 class App extends React.Component {
+
+  state = {
+    posts: [
+      {
+        nomeUsuario: 'paulinha',
+        fotoUsuario: 'https://picsum.photos/50/50',
+        fotoPost: 'https://picsum.photos/200/150?a=1'
+      },
+      {
+        nomeUsuario: 'Nyh Vignoli',
+        fotoUsuario: fotoPerfilAline,
+        fotoPost: 'https://picsum.photos/200/150?a=2'
+      },
+      {
+        nomeUsuario: 'Labenu',
+        fotoUsuario: logoLabenu,
+        fotoPost: 'https://picsum.photos/200/150?a=3'
+      }
+    ]
+  }
+
   render() {
+    const listaDePosts = this.state.posts.map((post)=>{
+      return (
+        <Post
+          nomeUsuario={post.nomeUsuario}
+          fotoUsuario={post.fotoUsuario}
+          fotoPost={post.fotoPost}
+        />
+      )
+    })
+
     return (
       <div className={'app-container'}>
-        <Post
+        {/* <Post
           nomeUsuario={'paulinha'}
           fotoUsuario={'https://picsum.photos/50/50'}
           fotoPost={'https://picsum.photos/200/150?a=1'}
@@ -24,7 +55,8 @@ class App extends React.Component {
           nomeUsuario={'Labenu'}
           fotoUsuario={logoLabenu}
           fotoPost={'https://picsum.photos/200/150?a=3'}
-        />
+        /> */}
+        {listaDePosts}
       </div>
     );
   }
