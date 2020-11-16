9d83904d04da6971c00d634543df84cb8d026559
Aline Vignoli <nyhv.contato@gmail.com>
2020-11-16 18:33:43 -0300

    criação de componentes BaseForm e ButtonsContainer

---
+++ semana10/labe-x/src/components/BaseForm.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+//Texto dos botões condicionalmente (login ou signup)
+const BaseForm = () => {
+    return (
+        <div>
+            Formulário base
+        </div>
+    )
+}
+
+export default BaseForm
---
+++ semana10/labe-x/src/components/ButtonsContainer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ButtonsContainer = () => {
+    return (
+        <div>
+            Grupo de botões
+        </div>
+    )
+}
+
+export default ButtonsContainer
