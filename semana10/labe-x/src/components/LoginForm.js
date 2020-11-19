ab2511b58c664176c5765e00b12c8c87ecd6facb
Aline Vignoli <nyhv.contato@gmail.com>
2020-11-19 20:59:45 -0300

    deleção de arquivos não utilizados

--- semana10/labe-x/src/components/LoginForm.js
+++ semana10/labe-x/src/components/LoginForm.js
@@ -1,35 +1,6 @@
 import React from 'react'
-import styled from 'styled-components'
 import useForm from '../hooks/useForm'
-
-const Form = styled.form`
-    width: 40%;
-    height: 100vh;
-    display: flex;
-    flex-direction: column;
-    gap: 10px;
-    margin: auto;
-    justify-content: center;
-` 
-
-const Input = styled.input`
-    font-size: 18px;
-    padding: 20px;
-    border-radius: 5px;
-`
-
-const Button = styled.button`
-    font-size: 18px;
-    padding: 20px;
-    background-color: rgba(48, 77, 120, 0.78);
-    border: 1px rgba(48, 77, 120, 0.78) solid;
-    border-radius: 5px;
-    color: #FFF;
-    &:hover {
-        background-color: rgba(11, 26, 49, 0.78);
-        cursor: pointer;
-    }
-`
+import { Form, Input, Button } from '../styles/formElementsStyles'
 
 const LoginForm = (props) => {
     const [form, handleForm] = useForm({email: '', password: ''})
