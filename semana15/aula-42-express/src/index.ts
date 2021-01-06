bfabee5bfbb5243c6e1f03f76899a6b335eb83ff
Aline Vignoli <nyhv.contato@gmail.com>
2021-01-06 13:35:14 -0300

    criei o endpoint getCountries

--- semana15/aula-42-express/src/index.ts
+++ semana15/aula-42-express/src/index.ts
@@ -1,10 +1,25 @@
-import express, {Express} from "express";
+import express, {Express, Request, Response} from "express";
 import cors from "cors";
+import {countries} from "./countries";
 
 const app: Express = express();
 
 app.use(express.json());
 app.use(cors());
+
+
+app.get("/countries/all", (req: Request, res: Response) => {
+
+    const result = countries.map(country => ({
+        id: country.id,
+        name: country.name
+    }))
+
+    res
+        .status(200)
+        .send(result)
+})
+
 
 app.listen(3003, () => {
     console.log("Server is running in http://localhost:3003")
