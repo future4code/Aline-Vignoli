8abab33711aebbbe2221926e33bf9bc684af9b11
Aline Vignoli <nyhv.contato@gmail.com>
2021-01-06 16:24:06 -0300

    criei o endpoint editCountry

--- semana15/aula-42-express/src/index.ts
+++ semana15/aula-42-express/src/index.ts
@@ -67,6 +67,40 @@ app.get("/countries/:id", (req: Request, res: Response) => {
     };
 });
 
+// ENDPOINT editCountry
+app.put("/countries/edit/:id", (req: Request, res:Response) => {
+    let errorCode: number = 400;
+
+    try {
+        const {name, capital} = req.body;
+        const index : number = countries.findIndex(
+            country => country.id === Number(req.params.id)
+        )
+
+        if (index === -1){
+            errorCode = 404
+            throw new Error("Id inválido")
+        }
+
+        if (!name){
+            errorCode = 400
+            throw new Error("Nome não foi informado")
+        }
+    
+        if (!capital){
+            errorCode = 400
+            throw new Error("Capital não foi informada")
+        }
+
+        countries[index].name = name;
+        countries[index].capital = capital;
+        res.status(200).send(req.body);
+    } catch (error) {
+        console.log(error)
+        res.status(errorCode).send(error.message);
+    }    
+})
+
 
 app.listen(3003, () => {
     console.log("Server is running in http://localhost:3003")
