8115e7b49fd779d20a7dbe28e2b9906373a3e960
Aline Vignoli <nyhv.contato@gmail.com>
2021-01-06 14:51:19 -0300

    criei o endpoint searchCountry

--- semana15/aula-42-express/src/index.ts
+++ semana15/aula-42-express/src/index.ts
@@ -9,15 +9,49 @@ app.use(cors());
 
 // ENDPOINT getAllCountries
 app.get("/countries/all", (req: Request, res: Response) => {
-
     const result = countries.map(country => ({
         id: country.id,
         name: country.name
     }));
 
-    res
-        .status(200)
-        .send(result);
+    res.status(200).send(result);
+});
+
+// ENDPOINT searchCountry
+app.get("/countries/search", (req: Request, res: Response) => {     
+    if (
+        !req.query.name &&
+        !req.query.capital &&
+        !req.query.continent
+    ) {
+        res.status(404).send("Nenhum parâmetro de busca foi informado!")
+    } else {
+        let result: country[] = countries;
+
+        if (req.query.name) {
+            result = result.filter(
+                country => country.name.includes(req.query.name as string)
+            );
+        };
+
+        if (req.query.capital) {
+            result = result.filter(
+                country => country.capital.includes(req.query.capital as string)
+            );
+        };
+
+        if (req.query.continent) {
+            result = result.filter(
+                country => country.continent.includes(req.query.continent as string)
+            );
+        };
+
+        if (result.length) {
+            res.status(200).send(result);
+        } else {
+            res.status(404).send("Not found");
+        };
+    };
 });
 
 // ENDPOINT getCountryById
@@ -27,11 +61,12 @@ app.get("/countries/:id", (req: Request, res: Response) => {
     );
 
     if (result){
-        res.status(200).send(result)
+        res.status(200).send(result);
     } else {
-        res.status(404).send("Not found")
+        res.status(404).send("Not found");
     };
 });
+
 
 app.listen(3003, () => {
     console.log("Server is running in http://localhost:3003")
