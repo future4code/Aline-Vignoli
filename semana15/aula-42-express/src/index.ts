d4e3d1fb84920ffe3bb8916ba65684bfab550908
Aline Vignoli <nyhv.contato@gmail.com>
2021-01-06 13:19:29 -0300

    configurações iniciais do projeto

---
+++ semana15/aula-42-express/src/index.ts
@@ -0,0 +1,11 @@
+import express, {Express} from "express";
+import cors from "cors";
+
+const app: Express = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.listen(3003, () => {
+    console.log("Server is running in http://localhost:3003")
+});
