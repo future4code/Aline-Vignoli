2f7e837ed4b0c94e9583ad4b99df87e9d39b9aef
Aline Vignoli <nyhv.contato@gmail.com>
2021-01-06 13:46:30 -0300

    criei o endpoint getCountryById

--- semana15/aula-42-express/src/index.ts
+++ semana15/aula-42-express/src/index.ts
@@ -1,25 +1,37 @@
 import express, {Express, Request, Response} from "express";
 import cors from "cors";
-import {countries} from "./countries";
+import {countries, country} from "./countries";
 
 const app: Express = express();
 
 app.use(express.json());
 app.use(cors());
 
-
+// ENDPOINT getAllCountries
 app.get("/countries/all", (req: Request, res: Response) => {
 
     const result = countries.map(country => ({
         id: country.id,
         name: country.name
-    }))
+    }));
 
     res
         .status(200)
-        .send(result)
-})
+        .send(result);
+});
 
+// ENDPOINT getCountryById
+app.get("/countries/:id", (req: Request, res: Response) => {
+    const result: country | undefined = countries.find(
+        country => country.id === Number(req.params.id)
+    );
+
+    if (result){
+        res.status(200).send(result)
+    } else {
+        res.status(404).send("Not found")
+    };
+});
 
 app.listen(3003, () => {
     console.log("Server is running in http://localhost:3003")
