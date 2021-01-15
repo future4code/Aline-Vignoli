190fe72f40b7f30e43113e2713d1f3b13ce47d05
Aline Vignoli <nyhv.contato@gmail.com>
2021-01-15 15:41:46 -0300

    criei o endpoint getUserById

--- semana16/todo-list/src/index.ts
+++ semana16/todo-list/src/index.ts
@@ -32,12 +32,22 @@ const createUser = async (
     };
 };
 
-// ENPOINTS
+const getUserById = async (id: string): Promise<any> => {
+    const result = await connection.raw(`
+      SELECT * FROM TodoListUser WHERE id = '${id}';
+    `)
+  
+    console.log(result[0][0])
+    return result[0][0]
+}
+
+// ENDPOINTS
 // createUser
 app.post("/user", async (req: Request, res: Response) => {
     let errorCode: number = 400;
     try {
         const { name, nickname, email } = req.body;
+
         if ( !name || !nickname || !email ) {
             errorCode = 422;
             throw new Error("Algum campo está faltando! Por favor, preencha corretamente.");
@@ -45,12 +55,7 @@ app.post("/user", async (req: Request, res: Response) => {
 
         const id = uuidv4();
 
-        createUser(
-            id,
-            name,
-            nickname,
-            email
-        );
+        await createUser(id, name, nickname, email);
   
         res.status(200).send({ 
             message: "Usuário criado com sucesso!",
@@ -64,6 +69,38 @@ app.post("/user", async (req: Request, res: Response) => {
     } catch (error) {
         res.status(errorCode).send({
             message: error.sqlMessage || error.message,
+        });
+    }
+});
+
+// getUserById
+app.get('/user/:id', async (req: Request, res: Response) => {
+    let errorCode: number = 400;
+    try {
+        const id = req.params.id as string;
+
+        if ( !req.params ) {
+            errorCode = 422;
+            throw new Error('Id do usuário não foi informado.')
+        }
+
+        const user = await getUserById(id);
+
+        if ( !user ) {
+            errorCode = 404;
+            throw new Error("Usuário não encontrado!")
+        }
+
+        res.status(200).send({ 
+            message: 'Success', 
+            user: {
+                id: id,
+                nickname: user.nickname
+            }
+        });
+    } catch (error) {
+        res.status(errorCode).send({
+            message: error.sqlMessage || error.message
         });
     }
 });
