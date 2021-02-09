507e902e3bbb36762cfd166ccb0ab3484cb573b7
Aline Vignoli <nyhv.contato@gmail.com>
2021-02-09 18:21:25 -0300

    teste 'vida do denfensor não deve ser afetada caso sua defesa seja igual ou maior a do atacante'

--- semana20/testes-unitarios-e-mock/tests/performAttack.test.ts
+++ semana20/testes-unitarios-e-mock/tests/performAttack.test.ts
@@ -1,5 +1,5 @@
 import { PerformAttackResult, performAttackDI } from "../src/performAttack";
-import { Character, validateCharacter } from "../src/validateCharacter";
+import { Character } from "../src/validateCharacter";
 
 describe("Testing performAttack", () => {
 
@@ -69,5 +69,38 @@ describe("Testing performAttack", () => {
             expect(validatorMockFalse).toHaveBeenCalledTimes(2);
             expect(validatorMockFalse).toHaveReturnedTimes(2);
         };
+    });
+
+    test("Should not affect defender's life", () => {
+        expect.assertions(4);
+        
+        const validatorMockTrue = jest.fn (()=> {
+            return true;
+        });
+
+        const attacker: Character = {
+            name: "Attacker",
+            life: 1500,
+            strength: 500,
+            defense: 100
+        };
+
+        const defender: Character = {
+            name: "Defender",
+            life: 1500,
+            strength: 500,
+            defense: 500
+        };
+
+        const output: PerformAttackResult = performAttackDI(
+            attacker, 
+            defender, 
+            validatorMockTrue
+        );
+
+        expect(output.defender.life).toBe(1500);
+        expect(validatorMockTrue).toHaveBeenCalled();
+        expect(validatorMockTrue).toHaveBeenCalledTimes(2);
+        expect(validatorMockTrue).toHaveReturnedTimes(2);
     });
 });
