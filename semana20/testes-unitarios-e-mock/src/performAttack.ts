4884d3111f0d4e51ddc628828c952a07ba05094b
Aline Vignoli <nyhv.contato@gmail.com>
2021-02-09 16:40:20 -0300

    implementando a função performAttack com inversão de dependências

--- semana20/testes-unitarios-e-mock/src/performAttack.ts
+++ semana20/testes-unitarios-e-mock/src/performAttack.ts
@@ -28,6 +28,35 @@ export const performAttack = (
     return result;
 };
 
+export const performAttackDI = (
+    attacker: Character,
+    defender: Character,
+    validator: (input: any)=> boolean
+): PerformAttackResult => {
+    const validAttacker = validator(attacker);
+    const validDefender = validator(defender);
+
+    if (!validAttacker || !validDefender) {
+        throw new Error("Invalid Character");
+    };
+
+    let updatedDefender: Character = defender;
+    const finalAttackValue: number = attacker.strength - defender.defense;
+    if (defender.defense <= attacker.strength) {
+        updatedDefender = {
+            ...defender,
+            life: defender.life - finalAttackValue
+        };
+    };
+
+    const result: PerformAttackResult = {
+        attacker,
+        defender: updatedDefender
+    };
+
+    return result;
+};
+
 interface PerformAttackResult {
     attacker: Character,
     defender: Character
