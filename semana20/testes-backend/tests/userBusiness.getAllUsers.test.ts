89c56a777b48a4796b967a52d3ee1849c892d805
Aline Vignoli <nyhv.contato@gmail.com>
2021-02-10 16:21:38 -0300

    teste de sucesso para função getAllUsers

--- semana20/testes-backend/tests/userBusiness.getAllUsers.test.ts
+++ semana20/testes-backend/tests/userBusiness.getAllUsers.test.ts
@@ -34,4 +34,44 @@ describe("Testing getAllUsers Business", () => {
             expect(error.message).toEqual("Only ADMIN users have access to this information");
         };
     });
+
+    test("Should return Users array", async () => {
+        expect.assertions(4);
+
+        userDatabase = { getAllUsers: jest.fn(() => {
+            return [{
+                id: "mockId",
+                name: "Mock User",
+                email: "mockuser@gmail.com",
+                password: "mockpassword",
+                role: UserRole.NORMAL
+            }]
+        })} as any;
+
+        tokenGenerator = { verify: jest.fn(() => { 
+            return { id: "mockId", role: UserRole.ADMIN }
+        })}
+
+        const userBusiness = new UserBusiness(
+            idGenerator,
+            hashGenerator,
+            userDatabase,
+            tokenGenerator
+        );
+
+        const output = await userBusiness.getAllUsers("mockToken");
+
+        expect(tokenGenerator.verify).toHaveReturnedWith(
+            { id: "mockId", role: UserRole.ADMIN }
+        );
+        expect(userDatabase.getAllUsers).toHaveBeenCalled();
+        expect(output.length).toBe(1);
+        expect(output).toEqual([{
+            id: "mockId",
+            name: "Mock User",
+            email: "mockuser@gmail.com",
+            password: "mockpassword",
+            role: UserRole.NORMAL
+        }]);   
+    });
 });
